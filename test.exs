65924afd3801debc4ddfa855b0588a5d2ef5adbf
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2023-08-18 00:01:51 +0700

    Add test script
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- test.exs
+++ test.exs
@@ -1,1 +1,15 @@
-String.to_atom("Hello")
+Mix.install([:dagger])
+
+client = Dagger.connect!()
+
+src = client |> Dagger.Client.host() |> Dagger.Host.directory(".")
+
+client
+|> Dagger.Client.container()
+|> Dagger.Container.from("returntocorp/semgrep")
+|> Dagger.Container.with_mounted_directory("/src", src)
+|> Dagger.Container.with_workdir("/src")
+|> Dagger.Container.with_exec(~w[semgrep --metrics=off --test rules])
+|> Dagger.Sync.sync()
+
+Dagger.close(client)
