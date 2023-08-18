b3140c4b928433aab3d965ac464831a84e87db01
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2023-08-18 09:33:47 +0700

    Add not is nil check
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- rules/nil.exs
+++ rules/nil.exs
@@ -1,9 +1,17 @@
 var = nil
 
-# ruleid: nil-check
+# ruleid: is-nil-check
 var == nil
 
-# ruleid: nil-check
+# ruleid: is-nil-check
 if var == nil do
   :ok
+end
+
+# ruleid: not-is-nil-check
+var != nil
+
+# ruleid: not-is-nil-check
+if var != nil do
+  :error
 end
--- rules/nil.fixed.exs
+++ rules/nil.fixed.exs
@@ -1,9 +1,17 @@
 var = nil
 
-# ruleid: nil-check
+# ruleid: is-nil-check
 is_nil(var)
 
-# ruleid: nil-check
+# ruleid: is-nil-check
 if is_nil(var) do
   :ok
+end
+
+# ruleid: not-is-nil-check
+not is_nil(var)
+
+# ruleid: not-is-nil-check
+if not is_nil(var) do
+  :error
 end
