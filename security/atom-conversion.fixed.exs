88ab9902c8a3b4cfea942cfd7d1eda0753f9b289
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2023-08-19 17:56:56 +0700

    security: autofixing String.to_atom/1
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- security/atom-conversion.fixed.exs
+++ security/atom-conversion.fixed.exs
@@ -1,14 +1,14 @@
 # ruleid: unsafe_string_to_atom
-String.to_atom("an_atom")
+String.to_existing_atom("an_atom")
 
 # ruleid: unsafe_string_to_atom_pipe
 "an_atom"
-|> String.to_atom()
+|> String.to_existing_atom()
 
 # ruleid: unsafe_string_to_atom_pipe
 "an_atom"
 |> String.upcase()
-|> String.to_atom()
+|> String.to_existing_atom()
 
 # ruleid: unsafe_list_to_atom
 List.to_atom(~c"Elixir")
