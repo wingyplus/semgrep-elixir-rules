4589628421c954e742e940588cc6ed7133a1f328
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2023-08-19 11:29:43 +0700

    Rename map-into rules
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- rules/enum.exs
+++ rules/enum.exs
@@ -1,13 +1,13 @@
-# ruleid: enum-map-enum-into-map
+# ruleid: map-into
 Enum.map(%{a: 1}, fn {k, v} -> {k, v * 2} end)
 |> Enum.into(%{})
 
-# ruleid: enum-map-enum-into-map
+# ruleid: map-into
 %{a: 1}
 |> Enum.map(fn {k, v} -> {k, v * 2} end)
 |> Enum.into(%{})
 
-# ruleid: enum-map-enum-into-map
+# ruleid: map-into
 %{a: 1}
 |> Enum.filter(fn {k, _v} -> k == :a end)
 |> Enum.map(fn {k, v} -> {k, v * 2} end)
